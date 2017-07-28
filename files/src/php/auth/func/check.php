a3de1c234488dc42e664b9d6b3a24f6c337581f2
taira <taira@taira>
2017-07-28 09:29:31 +0900

    remove ctrl/login.php

---
+++ files/src/php/auth/func/check.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * check wether access user is logged in.
+ * 
+ * @return (boolean) true : access user is logged in
+ * @return (boolean) false : access user is not logged in
+ */
+function isLoggedIn () {
+    try {
+        return false;
+    } catch (\Exception $e) {
+        throw new \Exception(
+                   PHP_EOL   .
+                   'File:'   . __FILE__   . ',' .
+                   'Line:'   . __line__   . ',' .
+                   'Func:' . __FUNCTION__ . ',' .
+                   $e->getMessage()
+              );
+    }
+}
+
