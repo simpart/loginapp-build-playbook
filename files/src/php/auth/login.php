4a361bfe3342dcaf45e8e8411da2e9344f62a086
taira <taira@taira>
2017-08-10 18:18:54 +0900

    add auth failed reason

--- files/src/php/auth/login.php
+++ files/src/php/auth/login.php
@@ -13,7 +13,8 @@ try {
     $jsn_post = file_get_contents('php://input');
     $post     = json_decode($jsn_post);
     $auth_ret = false;
-
+    $reason   = null;
+    
     $user = $post->user;
     $pass = $post->pass;
     
@@ -21,9 +22,11 @@ try {
     /* please add here            */
     /* about login authentication */
     /******************************/
-
+    $reason = 'auth function is not implements';
+    
     $Grtg_api_ret = array(
-        'login' => $auth_ret
+        'login'  => $auth_ret,
+        'reason' => $reason
     );
 } catch (\Exception $e) {
     throw $e;
